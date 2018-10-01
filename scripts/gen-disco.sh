581d8c4707b148664ca8342f31ddb775cbbf67f8
Jean Guyomarc'h <jean@guyomarch.bzh>
2018-10-01 13:05:19 +0200

    scripts: fix stm32 build.ninja generation

--- scripts/gen-disco.sh
+++ scripts/gen-disco.sh
@@ -4,4 +4,4 @@ set -e
 set -u
 set -x
 
-./nanokbs/nanokbs.py -a ./tests/target/disco.hjson -t ./tests/targets/build.ninja.j2 -o build.ninja
+./nanokbs/nanokbs.py -a ./tests/target/disco.hjson -t ./tests/target/build.ninja-disco.j2 -o build.ninja
